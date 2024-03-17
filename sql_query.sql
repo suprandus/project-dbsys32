194af785c07ca154d9a2e65e2e4f83b65f13fb40
Alberin Andus <130210722+suprandus@users.noreply.github.com>
2024-03-17 22:11:48 +0800

    Update sql_query.sql

--- sql_query.sql
+++ sql_query.sql
@@ -1,13 +1,15 @@
 USE dbsys32;
 
+
 -- tables
 CREATE TABLE user_account (
     [user_id] VARCHAR(5) PRIMARY KEY,
-	role_id int NOT NULL,
+    role_id INT NOT NULL,
     username VARCHAR(50) NOT NULL,
-    [password] VARCHAR(64) NOT NULL
-
+    [password] VARCHAR(256) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
 
 CREATE TABLE user_category(
 	role_id int PRIMARY KEY,
@@ -15,3 +17,12 @@ CREATE TABLE user_category(
 );
 
 select * from user_account
+
+-- service table
+CREATE TABLE services (
+    service_id VARCHAR(10) PRIMARY KEY,
+    service_name VARCHAR(255),
+    price DECIMAL(10, 2)
+);
+
+
